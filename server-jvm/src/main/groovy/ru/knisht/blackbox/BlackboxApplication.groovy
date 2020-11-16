b87b7ee2f5f2aa197972537d85b38e2483f6b811
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2020-11-16 03:11:12 +0300

    Enhance module system

---
+++ server-jvm/src/main/groovy/ru/knisht/blackbox/BlackboxApplication.groovy
@@ -0,0 +1,13 @@
+package ru.knisht.blackbox
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BlackboxApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(BlackboxApplication, args)
+    }
+
+}
---
+++ server-jvm/src/test/groovy/ru/knisht/blackbox/BlackboxApplicationTests.groovy
@@ -0,0 +1,13 @@
+package ru.knisht.blackbox
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlackboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
